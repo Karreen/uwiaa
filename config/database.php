6ad2eedfba259b0e4726b5017c9763c6c23cf6c0
Shane <shane.richards121@gmail.com>
2015-02-27 21:58:39 -0500

    deployed

--- config/database.php
+++ config/database.php
@@ -66,10 +66,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => env('Host', 'localhost'),
+			'database' => env('Database', 'forge'),
+			'username' => env('User', 'forge'),
+			'password' => env('Password', ''),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
--- resources/views/sessions/create.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Shane
- * Date: 2/23/2015
- * Time: 11:15 PM
- */
